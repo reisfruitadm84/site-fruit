994ef301bc1fb2ee2e55e02b0a3e80cecdd3b62f
reisfruitadm84 <reisfruitadm@gmail.com>
2025-10-09 07:37:47 -0300

    importandoarquivos

---
+++ script.js
@@ -0,0 +1,40 @@
+const btnIconMobile = document.querySelector('.icon-menu-list');
+const menuList = document.getElementById('navbar-mobile');
+const navLinks = document.querySelectorAll('.list');
+
+btnIconMobile.addEventListener(('click'), ()=> {
+    if(menuList.style.display == 'none'){
+        menuList.style.display = 'block';
+        
+    navLinks.forEach((link, index) => {  
+        link.style.animation = `effectTranslate 0.5s ease-in forwards ${
+        index / 18 + 0.05
+        }s`;
+    });
+
+    navLinks.style.backgroundColor = 'red';
+     
+    }
+    else {
+        menuList.style.display = 'none'; 
+        
+        navLinks.forEach((link, index) => {         
+                link.style = 'none';
+            });
+        }
+
+})
+
+function esconderMenu() {
+    menuList.style.display = 'none';
+}
+
+function verificarJanela(){
+    const larguraMinima = 790;
+
+    if (window.innerWidth >= larguraMinima){
+        esconderMenu();
+    }
+}
+
+window.addEventListener('resize', verificarJanela);
