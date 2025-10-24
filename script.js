d1104a91d02d3aef949fbcf7ec715a8c9ff3c48c
reisfruitadm84 <reisfruitadm@gmail.com>
2025-10-24 12:10:08 -0300

    inserindo descrição nos legumes e frutas

--- script.js
+++ script.js
@@ -166,13 +166,13 @@ window.addEventListener("resize", verificarJanela);
       { // ACEROLA
           img: "./img/frutas/acerola.png",  //Imagem do Produto
           nome: "Acerola",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "Rica em vitamina C e com sabor levemente ácido, a Acerola Reis Fruit é perfeita para sucos, vitaminas e sobremesas refrescantes. Colhida madura e congelada no ponto ideal, mantém cor vibrante e nutrientes essenciais.", //Descrição do Produto
           alt: "Acerola congelado",
       },
       { // AMORA
           img: "./img/frutas/amora.png",  //Imagem do Produto
           nome: "Amora",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "Com sabor adocicado e suave toque ácido, a Amora Reis Fruit é ideal para compotas, caldas, sobremesas e drinks. Fonte natural de antioxidantes, traz o melhor da fruta em sua forma mais prática e saborosa.", //Descrição do Produto
           alt: "Amora congelado",
       },
       { // COCO
@@ -184,43 +184,43 @@ window.addEventListener("resize", verificarJanela);
       { // GOIABA
           img: "./img/frutas/goiaba.png",  //Imagem do Produto
           nome: "Goiaba",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Goiaba Fatiada Reis Fruit mantém o sabor marcante e o aroma tropical da fruta fresca. Ideal para sucos, doces e sobremesas, é rica em vitamina C e fibras, com cortes padronizados que facilitam o preparo.", //Descrição do Produto
           alt: "goiaba congelado",
       },
       { // MANGA
           img: "./img/frutas/manga.png",  //Imagem do Produto
           nome: "Manga",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "Com doçura natural e textura cremosa, a Manga em Pedaços Reis Fruit é perfeita para smoothies, sobremesas e receitas tropicais. Congelada logo após o corte, conserva o sabor e o frescor da fruta madura.", //Descrição do Produto
           alt: "Manga congelado",
       },
       { // MIRTILO
           img: "./img/frutas/mirtilo.png",  //Imagem do Produto
           nome: "Mirtilo",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "O Mirtilo Reis Fruit é uma explosão de sabor e antioxidantes. Pequeno no tamanho e gigante nos benefícios, é ideal para bolos, iogurtes, caldas e bebidas. Frescor e qualidade premium em cada unidade.", //Descrição do Produto
           alt: "Mirtilo congelado",
       },
       { // UVA ISABEL
           img: "./img/frutas/uva_isabel.png",  //Imagem do Produto
           nome: "Uva Isabel",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Uva Isabel Reis Fruit oferece o sabor característico das uvas escuras, com doçura equilibrada e aroma marcante. Ideal para sucos, geleias e sobremesas, pronta para uso e sem perda de qualidade.", //Descrição do Produto
           alt: "Uva isabel congelada",
       },
       { // UVA VITÓRIA
           img: "./img/frutas/uva_vitoria.png",  //Imagem do Produto
           nome: "Uva Vitória",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Uva Vitória Reis Fruit combina praticidade e sabor intenso. Sem sementes, é perfeita para consumo direto, sobremesas, sucos e receitas gourmet. Textura firme e sabor naturalmente doce.", //Descrição do Produto
           alt: "Uva vitoria congelado",
       },
       { // SERIGUELA
           img: "./img/frutas/seriguela.png",  //Imagem do Produto
           nome: "Seriguela",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "Com sabor marcante e tropical, a Seriguela Reis Fruit é pura brasilidade em forma de fruta. Ideal para sucos, caldas e sobremesas, é rica em vitaminas e mantém o frescor natural mesmo após o congelamento.", //Descrição do Produto
           alt: "Seriguela congelada",
       },
       { // FRAMBOESA
           img: "./img/frutas/framboesa.png",  //Imagem do Produto
           nome: "Framboesa",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "Delicada e aromática, a Framboesa Reis Fruit é rica em antioxidantes e tem sabor levemente ácido. Perfeita para sobremesas, caldas e smoothies, traz cor e sofisticação a qualquer preparo.", //Descrição do Produto
           alt: "Framboesa congelada",
       },
     ];
@@ -229,73 +229,73 @@ window.addEventListener("resize", verificarJanela);
       { // BROCOLIS
           img: "./img/legumes/brocolis.png",  //Imagem do Produto
           nome: "Brocolis",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "O Brócolis Reis Fruit é selecionado e congelado logo após o corte para preservar cor, sabor e nutrientes. Ideal para acompanhamentos, tortas e refogados, mantendo textura firme e frescor de colheita o ano todo.", //Descrição do Produto
           alt: "brocolis congelado",
       },
       { // BATATA
           img: "./img/legumes/batata.png",  //Imagem do Produto
           nome: "Batata",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Batata em Cubos Reis Fruit é prática e versátil, ideal para sopas, purês, tortas e pratos do dia a dia. Congelada no ponto certo, mantém a textura e o sabor da batata fresca, pronta para ir direto à panela.", //Descrição do Produto
           alt: "Batata congelada",
       },
       { // CENOURA CUBO
           img: "./img/legumes/cenoura_cubo.png",  //Imagem do Produto
           nome: "Cenoura em Cubos",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Cenoura em Cubos Reis Fruit traz praticidade e cor para suas receitas. Rica em betacaroteno e fibras, é perfeita para refogados, sopas e acompanhamentos, mantendo o sabor natural e a textura crocante.", //Descrição do Produto
           alt: "Cenoura cubos congelada",
       },
       { // CENOURA BABY
           img: "./img/legumes/cenoura_baby.png",  //Imagem do Produto
           nome: "Cenoura Baby",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Cenoura Baby Reis Fruit é delicada e saborosa, congelada logo após a colheita para preservar o frescor. Ideal para saladas, acompanhamentos e pratos gourmet, une praticidade e apresentação impecável.", //Descrição do Produto
           alt: "Cenoura baby congelada",
       },
       { // ERVILHA
           img: "./img/legumes/ervilha.png",  //Imagem do Produto
           nome: "Ervilha",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Ervilha Reis Fruit é colhida fresca e congelada rapidamente para garantir cor vibrante, textura macia e sabor adocicado. Perfeita para arroz, sopas, purês e acompanhamentos saudáveis.", //Descrição do Produto
           alt: "Ervilha congelada",
       },
       { // MACAXEIRA
           img: "./img/legumes/macaxeira.png",  //Imagem do Produto
           nome: "Macaxeira",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Macaxeira Reis Fruit é descascada, cortada e congelada pronta para o preparo. Textura macia e sabor caseiro em cada pedaço. Ideal para cozidos, purês, caldos e pratos típicos do dia a dia.", //Descrição do Produto
           alt: "macaxeira congelada",
       },
       { // MILHO
           img: "./img/legumes/milho.png",  //Imagem do Produto
           nome: "Milho",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "O Milho Reis Fruit é doce, suculento e cheio de sabor natural. Congelado logo após a colheita, mantém o frescor e os nutrientes. Ideal para saladas, tortas, refogados e pratos rápidos.", //Descrição do Produto
           alt: "milho congelado",
       },
       { // VAGEM
           img: "./img/legumes/vagem.png",  //Imagem do Produto
           nome: "Vagem",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Vagem Reis Fruit é cortada e congelada no ponto certo, preservando cor, textura e nutrientes. Perfeita para saladas, refogados e acompanhamentos leves. Frescor garantido em cada porção.", //Descrição do Produto
           alt: "vagem congelada",
       },
       { // ESPINAFRE
           img: "./img/legumes/espinafre.png",  //Imagem do Produto
           nome: "Espinafre",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "O Espinafre Reis Fruit é selecionado e congelado fresco para preservar ferro, fibras e vitaminas. Ideal para omeletes, tortas, cremes e refogados. Pronto para uso e cheio de sabor natural.", //Descrição do Produto
           alt: "espinafre congelada",
       },
       { // SELETA PRIMAVERA
           img: "./img/legumes/seleta_primavera.png",  //Imagem do Produto
           nome: "Seleta Primavera",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Seleta Primavera Reis Fruit combina cenoura, brócolis, vagem, ervilha e milho em uma mistura colorida, nutritiva e prática. Perfeita para saladas, arroz, tortas e acompanhamentos. Sabor e equilíbrio em cada porção.", //Descrição do Produto
           alt: "seleta primavera congelada",
       },
       { // COUVE FLOR
           img: "./img/legumes/couve_flor.png",  //Imagem do Produto
           nome: "Couve Flor",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Couve-Flor Reis Fruit é delicada e nutritiva, congelada logo após o corte para preservar textura e sabor. Ideal para gratinados, purês e pratos leves, mantendo qualidade e praticidade no preparo.", //Descrição do Produto
           alt: "couve flor congelada",
       },
       { // JARDINEIRA
           img: "./img/legumes/jardineira.png",  //Imagem do Produto
           nome: "Jardineira",    //Nome do Produto
-          descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make", //Descrição do Produto
+          descricao: "A Jardineira Reis Fruit traz uma mistura clássica de batata, cenoura, vagem, ervilha e milho — colorida, nutritiva e prática. Pronta para o preparo de saladas, arroz, carnes e refogados. O equilíbrio ideal entre sabor e praticidade.", //Descrição do Produto
           alt: "jardineira congelada",
       },
     ];
